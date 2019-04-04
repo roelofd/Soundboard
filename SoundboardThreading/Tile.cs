28e2eff9cffa0e04c0ca2e3dabedd6994e69fcf2
siem schippers <siem.schippers@gmail.com>
2019-04-04 15:00:15 +0200

    Dynamic Grid

--- SoundboardThreading/Tile.cs
+++ SoundboardThreading/Tile.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace SoundboardThreading
@@ -14,12 +15,28 @@ namespace SoundboardThreading
         public Button playButton { get; set; }
         public Button downloadButton { get; set; }
 
-        public Tile(TextBox textBox, TextBlock textBlock, Button playButton, Button downloadButton)
+        public Tile(TextBox _textBox, TextBlock _textBlock, Button _playButton, Button _downloadButton)
         {
-            textBox = this.textBox;
-            textBlock = this.textBlock;
-            playButton = this.playButton;
-            downloadButton = this.downloadButton;
+            textBox = _textBox;
+            textBlock = _textBlock;
+            playButton = _playButton;
+            downloadButton = _downloadButton;
+
+            downloadButton.Click += Button_Click;
+        }
+
+        string fileLocation;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var url = new Uri(textBox.Text);
+            var downloader = new YoutubeDownloader();
+            fileLocation = downloader.Download(url.ToString());
+            downloadButton.Visibility = Visibility.Collapsed;
+            textBox.Visibility = Visibility.Collapsed;
+            playButton.Visibility = Visibility.Visible;
+            textBlock.Text = fileLocation.Split(".")[0];
+            textBlock.Visibility = Visibility.Visible;
         }
     }
 }
