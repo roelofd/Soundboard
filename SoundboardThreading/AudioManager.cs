fa30aaebbfbfba4576437f3d34045a78a175d3e6
Kevin Tabak <kevin_tabak@outlook.com>
2019-04-04 14:09:51 +0200

    Update AudioManager.cs
    Implement stop audio in mediaplayer.

--- SoundboardThreading/AudioManager.cs
+++ SoundboardThreading/AudioManager.cs
@@ -1,19 +1,32 @@
 ﻿using System;
+using System.Threading;
 using Windows.Storage;
+using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 
 namespace SoundboardThreading
 {
     class AudioManager
     {
+        private StorageFolder storageFolder;
+        private MediaElement playMusic;
+
+        public AudioManager()
+        {
+            storageFolder = ApplicationData.Current.LocalFolder;
+            playMusic = new Windows.UI.Xaml.Controls.MediaElement {AudioCategory = AudioCategory.Media};
+        }
+
         public async void Play(string fileName)
         {
-            var storageFolder = ApplicationData.Current.LocalFolder;
             var sound = await storageFolder.GetFileAsync(fileName);
-            var playMusic = new Windows.UI.Xaml.Controls.MediaElement();
-            playMusic.AudioCategory = AudioCategory.Media;
             playMusic.SetSource(await sound.OpenAsync(FileAccessMode.Read), sound.ContentType);
             playMusic.Play();
+        }
+
+        public void Stop()
+        {
+            playMusic.Stop();
         }
     }
 }
