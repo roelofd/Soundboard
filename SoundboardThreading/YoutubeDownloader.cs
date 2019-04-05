6237ea9b188628ddabcbb78244e9d3325dc0ad9f
roelofd <roelof.d@hotmail.com>
2019-04-05 11:05:57 +0200

    Added comments
    Added comments to the youtubedownloader class

--- SoundboardThreading/YoutubeDownloader.cs
+++ SoundboardThreading/YoutubeDownloader.cs
@@ -10,23 +10,30 @@ using Windows.UI.Popups;
 
 namespace SoundboardThreading
 {
+    // Class that handles the downloading of youtube videos
     class YoutubeDownloader
     {
 
-        private StorageFolder _storageFolder;
-        private YouTube _youTube;
-        private double progressPercentage = 0;
+        private StorageFolder _storageFolder;       // The folder where the downloaded files are stored
+        private YouTube _youTube;                   // Class used for getting videos
+        private double progressPercentage = 0;      // Percentage of the download progress
 
         public YoutubeDownloader()
         {
-            _storageFolder = ApplicationData.Current.LocalFolder;
-            _youTube = YouTube.Default;
+            _storageFolder = ApplicationData.Current.LocalFolder;   // Set download folder
+            _youTube = YouTube.Default;                             // Set Youtube class
         }
-        
+
+
+
+        /*
+         * Method called for downloading the actual video  
+         */
         public string Download(string url)
         {
-            YouTubeVideo video = _youTube.GetVideo(url);
+            YouTubeVideo video = _youTube.GetVideo(url);    // Get video by URL
 
+            // Check if video is encrypted
             if (video.IsEncrypted)
             {
                 var message = new MessageDialog($"{video.FullName} is encrypted!");
@@ -34,42 +41,53 @@ namespace SoundboardThreading
                 return null;
             }
 
-            WriteFileAsync(video);
-            return video.FullName + ".mp3";
+            WriteFileAsync(video);      
+            return video.FullName + ".mp3";     // Return the downloaded MP3
         }
 
+
+        /*
+         * Method for getting the current progress
+         */
         public double getProgress()
         {
             return progressPercentage;
         }
 
+        /*
+         * Methed for writing the video 
+         */
         private async void WriteFileAsync(YouTubeVideo video)
         {
-            StorageFile mp4StorageFile = await _storageFolder.CreateFileAsync(video.FullName, CreationCollisionOption.ReplaceExisting);
-            await FileIO.WriteBytesAsync(mp4StorageFile, video.GetBytes());
+            StorageFile mp4StorageFile = await _storageFolder.CreateFileAsync(video.FullName, CreationCollisionOption.ReplaceExisting); // Store the video as a MP4
+            await FileIO.WriteBytesAsync(mp4StorageFile, video.GetBytes());     
             
             StorageFile mp3StorageFile = await _storageFolder.CreateFileAsync(mp4StorageFile.Name + ".mp3", CreationCollisionOption.ReplaceExisting);
             var profile = MediaEncodingProfile.CreateMp3(AudioEncodingQuality.High);
-            await ToAudioAsync(mp4StorageFile, mp3StorageFile, profile);
+            await ToAudioAsync(mp4StorageFile, mp3StorageFile, profile);     // Convert the MP4 to MP3
         }
 
+        /*
+         * Method for converting the MP4 to MP3
+         */
         private async Task ToAudioAsync(StorageFile source, StorageFile destination, MediaEncodingProfile profile)
         {
-            var transcoder = new MediaTranscoder();
-            var prepareOp = await transcoder.PrepareFileTranscodeAsync(source, destination, profile);
+            var transcoder = new MediaTranscoder(); 
+            var prepareOp = await transcoder.PrepareFileTranscodeAsync(source, destination, profile);   // Prepare the file for conversion
 
             if (prepareOp.CanTranscode)
             {
-                var transcodeOp = prepareOp.TranscodeAsync();
+                var transcodeOp = prepareOp.TranscodeAsync();   // Conversion
 
-                transcodeOp.Progress +=
+                
+                transcodeOp.Progress +=                                         // Save progress
                     new AsyncActionProgressHandler<double>(TranscodeProgress);
-                transcodeOp.Completed +=
+                transcodeOp.Completed +=                                        // Save completion
                     new AsyncActionWithProgressCompletedHandler<double>(TranscodeComplete);
             }
             else
-            {
-                switch (prepareOp.FailureReason)
+            {   
+                switch (prepareOp.FailureReason)    // If failed, print failure
                 {
                     case TranscodeFailureReason.CodecNotFound:
                         System.Diagnostics.Debug.WriteLine("Codec not found.");
@@ -84,6 +102,9 @@ namespace SoundboardThreading
             }
         }
 
+        /*
+         * Method for displaying the progress
+         */
         private void TranscodeProgress(IAsyncActionWithProgress<double> asyncInfo, double percent)
         {
             // Display or handle progress info.
@@ -91,6 +112,9 @@ namespace SoundboardThreading
             System.Diagnostics.Debug.WriteLine(percent);
         }
 
+        /*
+         *  Method for displaying the completion
+         */
         private async void TranscodeComplete(IAsyncActionWithProgress<double> asyncInfo, AsyncStatus status)
         {
             asyncInfo.GetResults();
